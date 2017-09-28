b5ae59455b06a1ecb510bcb3dba81f6cf326a6cb
Matthew Heil <matthew.d.heil@gmail.com>
2017-09-28 17:25:07 +0000

    add lesson_5 practice problems

---
+++ 101_programming_foundations/lesson_5/pp5.rb
@@ -0,0 +1,14 @@
+munsters = {
+  "Herman" => { "age" => 32, "gender" => "male" },
+  "Lily" => { "age" => 30, "gender" => "female" },
+  "Grandpa" => { "age" => 402, "gender" => "male" },
+  "Eddie" => { "age" => 10, "gender" => "male" },
+  "Marilyn" => { "age" => 23, "gender" => "female"}
+}
+
+total_male_ages = 0
+munsters.values.each do |munster|
+  total_male_ages += munster['age'] if munster['gender'] == 'male'
+end
+
+p total_male_ages
