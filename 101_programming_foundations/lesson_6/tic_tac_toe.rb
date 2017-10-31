9b656fd53c79686599174024e6fcef2fadc37201
Matthew Heil <matthew.d.heil@gmail.com>
2017-10-31 13:28:56 +0000

    add tic_tac_toe

--- 101_programming_foundations/lesson_6/tic_tac_toe.rb
+++ 101_programming_foundations/lesson_6/tic_tac_toe.rb
@@ -1,6 +1,7 @@
-require 'pry'
-
+PLAYER_ONE = 'choose'
+PLAYERS = ['player', 'computer']
 INITIAL_MARKER = ' '
+MIDDLE_SQUARE = 5
 PLAYER_MARKER = 'X'
 COMP_MARKER = 'O'
 WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] +
@@ -59,20 +60,39 @@ def player_places_piece!(brd)
   brd[square] = PLAYER_MARKER
 end
 
-# refactor
-def computer_places_piece!(brd)
-  defend_line = WINNING_LINES.select do |line|
+def find_defensive_line(brd)
+  defensive_line = WINNING_LINES.select do |line|
     brd.values_at(*line).count(PLAYER_MARKER) == 2 &&
-      brd.values_at(*line).include?(' ')
+      brd.values_at(*line).include?(INITIAL_MARKER)
   end
 
-  if !!defend_line[0]
-    square = defend_line[0].select { |sqr| brd[sqr] == ' ' }[0]
-  else
+  defensive_line.flatten if !!defensive_line[0] # if defensive_line has a value
+end
+
+def find_offensive_line(brd)
+  offensive_line = WINNING_LINES.select do |line|
+    brd.values_at(*line).count(COMP_MARKER) == 2 &&
+      brd.values_at(*line).include?(INITIAL_MARKER)
+  end
+
+  offensive_line.flatten if !!offensive_line[0] # if offensive_line has a value
+end
+
+# rubocop:disable Metrics/AbcSize
+def computer_places_piece!(brd)
+  defend_line = find_defensive_line(brd)
+  attack_line = find_offensive_line(brd)
+  if !defend_line && !attack_line
     square = empty_squares(brd).sample
+    square = MIDDLE_SQUARE if brd[MIDDLE_SQUARE] == INITIAL_MARKER
+  elsif !attack_line
+    square = defend_line.select { |sqr| brd[sqr] == INITIAL_MARKER }.first
+  else
+    square = attack_line.select { |sqr| brd[sqr] == INITIAL_MARKER }.first
   end
   brd[square] = COMP_MARKER
 end
+# rubocop:enable Metrics/AbcSize
 
 def board_full?(brd)
   empty_squares(brd) == []
@@ -104,24 +124,63 @@ def joinor(arr, delimiter = ', ', joining_word = 'or')
   end
 end
 
+def take_turn!(active_player, brd)
+  if active_player == 'player'
+    player_places_piece!(brd)
+  else
+    computer_places_piece!(brd)
+  end
+end
+
+def choose_player_one
+  choice = ''
+  loop do
+    prompt "Would you like to go first? (y/n)"
+    choice = gets.chomp.downcase[0]
+    if choice == 'y' || choice == 'n'
+      break
+    else
+      prompt "Please enter a valid choice of yes or no."
+    end
+  end
+  if choice == 'y'
+    'player'
+  else
+    'computer'
+  end
+end
+
+def alternate_player(current_player)
+  PLAYERS.select { |player| player != current_player }.first
+end
+
 loop do
   player_wins = 0
   computer_wins = 0
+  player_one = PLAYER_ONE
+  player_two = ''
 
   loop do
     board = initialize_board
+    player_one = choose_player_one if player_one == 'choose'
+    player_two = PLAYERS.select { |player| player != player_one }.first
+    current_player = player_one
 
+    # rubocop:disable Style/IfInsideElse
     loop do
       display_board(board, player_wins, computer_wins)
 
-      player_places_piece!(board)
-      player_wins += 1 if detect_winner(board) == "Player"
-      break if someone_won?(board) || board_full?(board)
-
-      computer_places_piece!(board)
-      computer_wins += 1 if detect_winner(board) == "Computer"
+      take_turn!(current_player, board)
+      display_board(board, player_wins, computer_wins)
+      if current_player == 'player'
+        player_wins += 1 if detect_winner(board) == "Player"
+      else
+        computer_wins += 1 if detect_winner(board) == "Computer"
+      end
+      current_player = alternate_player(current_player)
       break if someone_won?(board) || board_full?(board)
     end
+    # rubocop:enable Style/IfInsideElse
 
     display_board(board, player_wins, computer_wins)
 
