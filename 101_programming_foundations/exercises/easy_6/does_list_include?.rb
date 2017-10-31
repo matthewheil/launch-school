b0e531e9da9698818a1d349d4b084bed2da6b70d
Matthew Heil <matthew.d.heil@gmail.com>
2017-10-31 14:12:46 +0000

    add easy_6 exercises

---
+++ 101_programming_foundations/exercises/easy_6/does_list_include?.rb
@@ -0,0 +1,11 @@
+def include?(arr, search_term)
+  arr.each { |el| return true if el == search_term }
+  false
+end
+
+p include?([1,2,3,4,5], 3) == true
+p include?([1,2,3,4,5], 6) == false
+p include?([], 3) == false
+p include?([nil], nil) == true
+p include?([], nil) == false
+
