514f7b84b9d1f1d719ccb94737d8bda85d8d3857
Matthew Heil <matthew.d.heil@gmail.com>
2017-11-06 15:46:09 +0000

    updated swapcase

--- 101_programming_foundations/exercises/easy_7/swap_case.rb
+++ 101_programming_foundations/exercises/easy_7/swap_case.rb
@@ -15,5 +15,21 @@ def swapcase(string)
   end.join(' ')
 end
 
+def another_way(string)
+  characters = string.chars.map do |char|
+    if UPPERCASE.include?(char)
+      char.downcase
+    elsif LOWERCASE.include?(char)
+      char.upcase
+    else
+      char
+    end
+  end
+  characters.join
+end
+
 p swapcase('CamelCase') == 'cAMELcASE'
 p swapcase('Tonight on XYZ-TV') == 'tONIGHT ON xyz-tv'
+
+p another_way('CamelCase') == 'cAMELcASE'
+p another_way('Tonight on XYZ-TV') == 'tONIGHT ON xyz-tv'
